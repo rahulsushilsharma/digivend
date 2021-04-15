2fccd24eddabb783b85b75c128e87147e0212427
Rahul Sharma <rahu8299@gmail.com>
2021-04-15 23:57:27 +0530

    adding ico + creating login / signup form

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -12,6 +12,8 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+var db = firebase.firestore();
+
 var provider = new firebase.auth.GoogleAuthProvider();
 
 function googleSignin() {
@@ -32,8 +34,6 @@ function googleSignin() {
       modal.style.display = "none";
 
       // adding profile pic
-      document.getElementById("user-img").src = user.providerData[0].photoURL;
-
     })
     .catch(function (error) {
       var errorCode = error.code;
@@ -51,11 +51,119 @@ function googleSignout() {
 
     .then(
       function () {
-        document.getElementById("user-img").src = "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png";
-        console.log("Signout Succesfull",  document.getElementById("user-img").src);
+        document.getElementById("user-img").src =
+          "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png";
+        console.log(
+          "Signout Succesfull",
+          document.getElementById("user-img").src
+        );
       },
       function (error) {
         console.log("Signout Failed");
       }
     );
 }
+
+function callDbForMainPage() {
+  db.collection("main_page")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        renderUI(doc.data());
+      });
+    });
+}
+
+// UI render
+
+function renderUI(data) {
+  let main = document.getElementById("main");
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+  
+    <a href="./product-type/product-type.html?">
+      <img src="${data.img}">
+        <div class="prod-disc">
+          <p class="disc-line">Shirts & Trousers</p>
+          <p class="offer-line">Upto 70% Off</p>
+          <p class="prmo-line">Peter England, Arrow, Mufti...</p>
+        </div>
+    </a>
+
+`;
+  main.appendChild(card);
+}
+
+const searchInput = document.getElementById("search-input");
+
+function search() {
+  console.log(searchInput.value, window.location);
+  window.location = "search/search.html?" + searchInput.value;
+  let main = document.getElementById("main");
+  main.innerHTML = `<h2>${searchInput.value}</h2>`;
+}
+
+searchInput.addEventListener("keyup", (ev) => {
+  if (ev.key == "Enter") search();
+});
+
+callDbForMainPage();
+
+// let email = "nuts@gmail.com";
+// let password = "thisnuts#123";
+
+// firebase
+//   .auth()
+//   .createUserWithEmailAndPassword(email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     var user = userCredential.user;
+//     // ...
+//     console.log(user);
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorCode, errorMessage);
+//     // ..
+//   });
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log(user);
+    document.getElementById("user-img").src = user.photoURL;
+    document.getElementById("nav_login").style.display = "none";
+    document.getElementById("nav_logout").style.display = "block";
+  } else {
+    document.getElementById("nav_login").style.display = "block";
+    document.getElementById("nav_logout").style.display = "none";
+    console.log("not signed in");
+  }
+});
+
+document.getElementById("form_login").addEventListener(
+  "click",
+
+  () => {
+    let email = "nuts@gmail.com";
+    let password = "thisnuts#123";
+
+    firebase
+      .auth()
+      .signInUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+  }
+);
--- public/js/utility.js
+++ public/js/utility.js
@@ -1,9 +1,40 @@
-
-var modal = document.getElementById('id01');
+var modal = document.getElementById("id01");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+const themeMap = {
+  light: "solar",
+  dark: "light",
+  solar: "dark",
+};
+
+const theme =
+  localStorage.getItem("theme") ||
+  ((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem("theme");
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem("theme", next);
 }
+
+console.log("common js works");
+
+// Search Functions
+document.getElementById('login_tab_btn').addEventListener('click',()=>{
+  document.getElementById('login_tab').style.display = 'block'
+  document.getElementById('signin_tab').style.display = 'none'
+})
+document.getElementById('signin_tab_btn').addEventListener('click',()=>{
+  document.getElementById('login_tab').style.display = 'none'
+  document.getElementById('signin_tab').style.display = 'block'
+})
